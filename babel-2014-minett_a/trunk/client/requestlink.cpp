22d9ab43351c75d027fd07549a8efb88b23535d6
minette.alexandre <minette.alexandre@gmail.com>
2011-12-05 21:48:06 +0000

    request link features implement

--- babel-2014-minett_a/trunk/client/requestlink.cpp
+++ babel-2014-minett_a/trunk/client/requestlink.cpp
@@ -1,12 +1,6 @@
 #include "requestlink.h"
 #include "connecttome.h"
 #include "accountmanager.h"
-#include "ListenServer.h"
-#include "CentralWidget.h"
-#ifdef OS_UNIX
-#include <sys/time.h>
-#include <unistd.h>
-#endif
 
 RequestLink::RequestLink()
 {
@@ -22,17 +16,15 @@ bool RequestLink::createServerSockMiam(std::string const &login)
     if (1)
     {
       _net->getSocket()->createServerSocket(IPortableSocket::TCP, 7536);
-      PortableNetworkManagerSingle::getInstance()->addNetwork(_net);
-      _type = IPortableSocket::TCP;
+     _type = IPortableSocket::TCP;
      _port = 7536;
       short unsigned int id = Protocol::getInstance()->getCurrentReplyId();
      std::cout << "RequestLink::createServerSock() : id = " << id << std::endl;
-     TestConnectionSingleton::getInstance()->send("", IPortableSocket::TCP, 7536, false);
+     TestConnectionSingleton::getInstance()->send(login, IPortableSocket::TCP, 7536, false);
      std::cout << "RequestLink::createServerSock() :: registring packet..." << std::endl;
-     Protocol::getInstance()->registerPacketId(id, "", Protocol::getInstance()->getDefaultGetaway(),
+     Protocol::getInstance()->registerPacketId(id, login, Protocol::getInstance()->getDefaultGetaway(),
                                               RequestLinkSingleton::getInstance(),
                                               reinterpret_cast<Protocol::SlotCall> (&RequestLink::testConnection),
-                                              login,
                                               Protocol::DEFAULT_TIMEOUT);
      std::cout << "RequestLink::createServerSock() :: DONE..." << std::endl;
 return (false);
@@ -41,21 +33,12 @@ return (false);
     return (true);
 }
 
-void RequestLink::calling(bool timeout, Packet *p)
+void RequestLink::calling(bool timeout, Packet *)
 {
-    std::cout << "RequestLink::calling: THIS FUNCTION IS THE GOAL OF THE PROJECT !! :P" << std::endl;
     if (timeout)
     {
-        ListenServerSingleton::getInstance()->emitCallFail();
-        return ;
+
     }
-    if (p == NULL || p->getLogin() == "")
-    {
-    }
-    else {
-     CentralWidgetSingleton::getInstance()->emitNeedOpenTalkWindow(p->getLogin().c_str());
-    }
-    //    emin callSuccess(login); //throw popup
 }
 
 bool RequestLink::createNewLink(std::string const &login)
@@ -77,30 +60,22 @@ void RequestLink::testConnection(bool timeout, Packet *p)
 {
     if (timeout || !p || p->getStatus() == Protocol::FAILED)
     {
-        std::cout << "RequestLink::testConnection() : no response from host timeout=" << timeout  << std::endl;
+        std::cout << "RequestLink::testConnection() : no response from host timeout=(" << timeout << ")" << std::endl;
         Protocol::getInstance()->send(p->getLogin(), Protocol::REQUEST_CONNECTION, NULL, 0);
         return ;
     }
     _serverSockExist = true;
     std::cout << "here:RequestLink::testConnection();" << std::endl;
-    std::cout << "RequestLink::testConnection() len = " << p->getLen() << std::endl;
     if (p->getData() != NULL && p->getLen() >= sizeof(Protocol::Status) + 4)
     {
         std::cout << "{<=>}" << "HELLO WORLD" << std::endl;
         NetworkRouteSingleton::getInstance()->setIp
                 (*(reinterpret_cast<unsigned int *> (static_cast<char *> (p->getData()) + sizeof(Protocol::Status))));
-        _net->setName(p->getReturningLogin());
-        ConnectToMe::getInstance()->sendConnnectToMe(p->getReturningLogin(),
+        ConnectToMe::getInstance()->sendConnnectToMe(p->getLogin(),
                                                      _type,
                                                      NetworkRouteSingleton::getInstance()->getuIp(),
                                                      _port);
-        unsigned int id = Protocol::getInstance()->getCurrentReplyId();
-        Protocol::getInstance()->send(p->getReturningLogin(), Protocol::CALL, "", 0, id, false);
-        std::cout << "registring id:" << id << std::endl;
-        Protocol::getInstance()->registerPacketId(id, p->getReturningLogin(),
-                                                  p->getNetwork(), this,
-                                                  reinterpret_cast<Protocol::SlotCall> (&RequestLink::calling), p->getReturningLogin(),
-                                                  20000);
+        PortableNetworkManagerSingle::getInstance()->addNetwork(_net);
     }
 
 }
